422bdcd6f90abe3bffd79dbf2a6f168cb59c458c
RishabhMathur06 <rishabhmathur612@gmail.com>
2025-01-15 13:36:26 +0530

    added string question

---
+++ Array&Strings/String_compression.py
@@ -0,0 +1,12 @@
+class Solution:
+    def compress(self, chars, n):
+        pass
+
+if __name__ == "__main__":
+    
+    ob = Solution()
+
+    chars = ["a","a","b","b","c","c","c"]
+    n = len(chars)
+
+    ob.compress(chars, n)
